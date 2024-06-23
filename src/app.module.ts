e6059e16a89ee74046b568aebed2f36a155e4726
= <=>
2024-06-23 16:26:03 -0500

    Update README

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { JoiValidationSchema } from './config/joi.validation';
       rootPath: join(__dirname, '..', 'public'),
     }),
     MongooseModule.forRoot(process.env.MONGODB, {
-      dbName: 'pokemonsdb'
+      dbName: 'pokemonsdb',
     }),
     PokemonModule,
     CommonModule,
